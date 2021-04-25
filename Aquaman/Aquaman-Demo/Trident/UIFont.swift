e2360155e5cda7458c7db94b8437bedc89d8a75f
邓江洲 <dengjiangzhouReal@gmail.com>
2021-04-25 10:34:55 +0800

    framework   Trident

---
+++ Aquaman/Aquaman-Demo/Trident/UIFont.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont.swift
+//  Trident
+//
+//  Created by bawn on 2020/3/17.
+//  Copyright © 2020 bawn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    var weightValue: Float {
+        guard let value = traits[.weight] as? NSNumber else {
+            return 0
+        }
+        return value.floatValue
+    }
+    
+    private var traits: [UIFontDescriptor.TraitKey: Any] {
+        return fontDescriptor.object(forKey: .traits) as? [UIFontDescriptor.TraitKey: Any] ?? [:]
+    }
+}
